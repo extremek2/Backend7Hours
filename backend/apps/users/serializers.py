3eb8bd1e5fcf4b4faa3968810cd59d973050e930
jhj1111 <happyijun@naver.com>
2025-11-12 17:59:40 +0900

    fix(user): nickname 필수로 변경,  토큰 이메일 방식으로 변경

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,8 +1,41 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 User = get_user_model()
+
+class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('username', None)
+        self.fields['email'] = serializers.EmailField()
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("No user with this email was found.")
+
+        if not user.check_password(password):
+            raise serializers.ValidationError("Incorrect password.")
+
+        if not user.is_active:
+            raise serializers.ValidationError("User is disabled.")
+
+        # The user is valid, now we can generate the tokens
+        refresh = self.get_token(user)
+
+        data = {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+            'user': UserSerializer(user).data, # Add user info to the response
+        }
+
+        return data
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
@@ -10,7 +43,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'full_name', 'email', 'password', 'password_verification']
+        fields = ['username', 'nickname', 'full_name', 'email', 'password', 'password_verification']
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password_verification']:
@@ -21,6 +54,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         validated_data.pop('password_verification')
         user = User.objects.create(
             username=validated_data['username'],
+            nickname=validated_data.get('nickname', ''),
             full_name=validated_data.get('full_name', ''),
             email=validated_data['email'],
         )
