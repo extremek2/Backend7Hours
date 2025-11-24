b22e8df862ed36be06a7901a6d11847c85222bbe
jhj1111 <happyijun@naver.com>
2025-11-24 20:28:48 +0900

    fix(pets): pets birthday json parser 오류 해결

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -113,6 +113,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',  # IsAuthenticated
     ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ],
 }
 
 
